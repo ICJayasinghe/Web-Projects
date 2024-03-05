72ea6cad8d2034b4a2b151b0bb49c7e4aad0645c
Isuranga Charuka Jayasinghe <121102527+ICJayasinghe@users.noreply.github.com>
2024-03-05 19:12:02 +0530

    Update

---
+++ angular-beginner/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TodoComponent } from './todo/todo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    ProfileComponent,
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
